361f64c42311dbaed5d94a990fda011400e4b25a
Andrew Chen <chuenchen309@gmail.com>
2025-03-21 17:07:59 +0800

    .

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-CONTAINER="fastapi-example"
-DOCKER_HUB_USERNAME="edgar"
+CONTAINER="fastapi-example-api"
+DOCKER_HUB_USERNAME="chuenchen309"
 DOCKER_REPO="$DOCKER_HUB_USERNAME/$CONTAINER"
 VERSION="1.0.1"
 
-MESSAGE="[edgar]"
+MESSAGE="[Andrew]"
 
 docker build -t $DOCKER_REPO:$VERSION .
 docker push $DOCKER_REPO:$VERSION
